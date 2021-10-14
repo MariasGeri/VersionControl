186e9c3d0e8cf233980dd15c4c3432b9ef0ec47a
mariasg <gergely.marias@multisoft.hu>
2021-10-14 15:22:16 +0200

    1

---
+++ UserMaintence/UserMaintence/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserMaintence.Entities
+{ 
+
+	public class User
+	{
+		public User()
+		{
+			public Guid ID { get; set; } = Guid.NewGuid();
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public string FullName { get { return string.Format("{0}{1}", LastName, FullName); } }
+		}
+	}
+}
