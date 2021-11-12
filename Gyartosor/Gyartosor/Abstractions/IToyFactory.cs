607e5441bf2d8a0d74a993a936cd961e9ce7ff40
Gergely Máriás <gergely.marias@stud.uni-corvinus.hu>
2021-11-12 17:54:27 +0100

    finish

---
+++ Gyartosor/Gyartosor/Abstractions/IToyFactory.cs
@@ -0,0 +1,15 @@
+﻿using Gyartosor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyartosor.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+
+    }
+}
